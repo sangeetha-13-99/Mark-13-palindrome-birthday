4c59c5ad9081b9c3e84b741367b396b4e1cf9bdf
Sangeetha Jula <74001727+sangeetha-13-99@users.noreply.github.com>
2022-08-31 00:06:18 +0530

    adding footer section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./styles.css";
 import Main from './Main';
 import Header from './Header';
+import Footer from './Footer'
 export default function App() {
   return (
     <div className="App">
       <Header/>
       <Main />
+      <Footer />
     </div>
   );
 }
